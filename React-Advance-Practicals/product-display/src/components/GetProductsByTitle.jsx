468f44f0c3ff427fc63a075143e5dd008a3b1a57
AT-dev-dds <ashutoshmishra.dds@gmail.com>
2026-04-13 12:05:26 +0530

    feat: implement search functionality in GetProductsByTitle and refactor useProducts for API fetching

--- React-Advance-Practicals/product-display/src/components/GetProductsByTitle.jsx
+++ React-Advance-Practicals/product-display/src/components/GetProductsByTitle.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useProducts from '../services/GET-API/useProducts.js'
 export default function GetProductsByTitle() {
 
-  const {products,error,isLoading}=useProducts();
+  const [search,setSearch]=useState("");
+
+  const {products,error,isLoading}=useProducts(search);
 
   if(isLoading) return <h1>Loading....</h1>;
   if(error) return <h3>{error.message}</h3>;
 
   return (
    <>
+
+   <input type="text" placeholder='Search' value={search} onChange={(e)=>setSearch(e.target.value)} />
    {
     products.map((product)=><div key={product.id}>
       <h1>{product.title}</h1>
+      <p>{product.category}</p>
     </div>)
    }
+
    </>
   )
+  
 }
--- React-Advance-Practicals/product-display/src/services/GET-API/useProducts.js
+++ React-Advance-Practicals/product-display/src/services/GET-API/useProducts.js
@@ -1,17 +1,29 @@
 import React, { useState,useEffect } from 'react'
 import axios from 'axios'
-export default function useProducts() {
+
+export default function useProducts(search) {
 
     const [products,setProducts]=useState([]);
     const [error,setError]=useState(null);
     const [isLoading,setIsLoading]=useState(true);
+   
+    
 
     useEffect(()=>{
+
+      const controller=new AbortController();
     const fetchProducts=async()=>{
        try{
-        const res=await axios.get("https://dummyjson.com/products");
+        const res=await axios.get(`https://dummyjson.com/products/search?q=${search}`,
+          {
+            signal:controller.signal
+          }
+        );
         setProducts(res.data.products);
        }catch(err){
+        if(axios.isCancel(err)){
+             return;
+        }
          setError(err);
        }
        finally{
@@ -19,7 +31,12 @@ export default function useProducts() {
        }
     };
     fetchProducts();
-    },[])
+    
+    return ()=>{
+      controller.abort();
+    }
+
+    },[search])
 
 
   return {products,error,isLoading} ;
