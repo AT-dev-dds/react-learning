63ac3b72868f0529e93c8399071211ba5df832b1
AT-dev-dds <ashutoshmishra.dds@gmail.com>
2026-04-14 12:26:58 +0530

    fix: update form submission handler and input types in Form component

--- React-Advance-Practicals/react-hook-form/src/component/Form.jsx
+++ React-Advance-Practicals/react-hook-form/src/component/Form.jsx
@@ -8,30 +8,30 @@ export default function Form() {
         handleSubmit,
         formState:{errors}
     }= useForm();
-    
 
-    const onClick=(data)=>{
+
+    const onSubmit=(data)=>{
         console.log("Button Clicked!",data)
     }
 
   return (
     <>
-    <form  action="" onSubmit={handleSubmit(onClick)}>
+    <form  action="" onSubmit={handleSubmit(onSubmit)}>
      
      <div className='mb-3'>
        <label htmlFor=""> Email</label>
-       <input type="text" {...register("email",{required:true})} />
+       <input type="email" {...register("email",{required:true})} />
      </div>
 
    
 
        <div className='mb-3'>
        <label htmlFor=""> Password</label>
-       <input type="text" {...register("password",{required:true,minLength:{value:6,
+       <input type="password" {...register("password",{required:true,minLength:{value:6,
        message:"Password must be more than 6 characters" }})} />
      </div>
      
-       {errors.password && <p>{errors.password.message}</p>}
+       {errors.password && <p style={{color:"red"}}>{errors.password.message}</p>}
 
      <div className="mb-3">
         <button type="submit">Submit</button>
