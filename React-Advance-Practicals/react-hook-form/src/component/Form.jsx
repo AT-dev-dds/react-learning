68d03dfefee26dc122c0e99574bd19a305d29b68
AT-dev-dds <ashutoshmishra.dds@gmail.com>
2026-04-14 12:49:11 +0530

    enhance password validation messages and structure in Form component

--- React-Advance-Practicals/react-hook-form/src/component/Form.jsx
+++ React-Advance-Practicals/react-hook-form/src/component/Form.jsx
@@ -23,16 +23,26 @@ export default function Form() {
        <input type="email" {...register("email",{required:true})} />
      </div>
 
+
    
 
        <div className='mb-3'>
        <label htmlFor=""> Password</label>
-       <input type="password" {...register("password",{required:true,minLength:{value:6,
-       message:"Password must be more than 6 characters" }})} />
+       <input type="password" {...register("password",{required:true,
+        validate:{
+            minLength: (value)=>value.length>=6,
+            maxLength:(value)=>value.length<=14
+        }
+       })} />
      </div>
      
-       {errors.password && <p style={{color:"red"}}>{errors.password.message}</p>}
-
+     { 
+        errors.password?.type==="minLength" && <p style={{color:"red"}}>Password Should not be less than 6</p>
+        
+     }
+     {
+        errors.password?.type==="maxLength" && <p style={{color:"darkblue"}}>Password must be atmost 14 Characters </p>
+     }
      <div className="mb-3">
         <button type="submit">Submit</button>
      </div>
